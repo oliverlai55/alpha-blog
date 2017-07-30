c199ac12f988bf40d57089d60f88ac69dba31a24
Rails Udemy Course <railsudemycourse@gmail.com>
2017-07-30 21:44:39 +0000

    Create show action and add gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = 'Wecome to the alpha blog #{@user.username}'
+      flash[:success] = "Wecome to the alpha blog #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
@@ -25,6 +25,10 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
   
   private
